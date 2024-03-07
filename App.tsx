3c3497722b061fab3f6470ed485b505489ef0c76
anthony-mini <anthony-mini@outlook.fr>
2024-03-07 10:00:29 +0100

    Ajouter l'écran de démarrage et la logique de chargement des ressources

--- App.tsx
+++ App.tsx
@@ -1,5 +1,22 @@
+import 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { StatusBar } from 'expo-status-bar';
+
 import Navigation from './src/navigation';
+import { useLoadedAssets } from './src/hooks/useLoadedAssets';
 
 export default function App() {
-  return <Navigation />;
+  const isLoadingComplete = useLoadedAssets();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation />
+        <StatusBar style="dark" />
+      </SafeAreaProvider>
+    );
+  }
 }
