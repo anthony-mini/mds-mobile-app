d7eafa69c9765d8a478989fda599c43f7b2aecd7
anthony-mini <anthony-mini@outlook.fr>
2024-03-26 16:21:48 +0100

    fix gen filter

--- src/components/GridList/index.tsx
+++ src/components/GridList/index.tsx
@@ -30,7 +30,7 @@ const GridList = () => {
       setData(res);
       setIsLoading(false);
     });
-  }, []);
+  }, [gen]);
 
   return (
     <SafeAreaView>
@@ -56,110 +56,93 @@ const GridList = () => {
             </Picker>
           </View>
 
-          {data.map(
-            (
-              {
-                pokedex_id,
-                generation,
-                category,
-                name,
-                sprites,
-                types,
-                talents,
-                stats,
-                resistances,
-                evolution,
-                height,
-                weight,
-                egg_groups,
-                sexe,
-                catch_rate,
-                level_100,
-                forme,
+          {data &&
+            data.map(
+              (
+                {
+                  pokedex_id,
+                  generation,
+                  name,
+                  sprites,
+                  types,
+                  height,
+                  weight,
+                },
+                index,
+              ) => {
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() => {
+                      // handle onPress
+                    }}
+                  >
+                    <View style={styles.card}>
+                      <View style={styles.cardTop}>
+                        <Image
+                          alt=""
+                          resizeMode="cover"
+                          style={styles.cardImg}
+                          source={{ uri: sprites?.regular }}
+                        />
+                      </View>
+
+                      <View style={styles.cardBody}>
+                        <View style={styles.cardHeader}>
+                          <Text style={styles.cardTitle}>{name?.fr}</Text>
+
+                          <Text style={styles.cardPrice}>
+                            Pokedex ID: {pokedex_id}
+                          </Text>
+                        </View>
+
+                        <View style={styles.cardStats}>
+                          <View style={styles.cardStatsItem}>
+                            <FeatherIcon color="#48496c" name="zap" size={14} />
+
+                            <Text style={styles.cardStatsItemText}>
+                              Generation: {generation}
+                            </Text>
+                          </View>
+
+                          <View style={styles.cardStatsItem}>
+                            {types?.map((type, index) => (
+                              <View
+                                key={index}
+                                style={{
+                                  flexDirection: 'row',
+                                  alignItems: 'center',
+                                  marginRight: 3,
+                                  marginLeft: 3,
+                                }}
+                              >
+                                <Image
+                                  source={{ uri: type.image }}
+                                  style={styles.cardTypeImg}
+                                />
+                                <Text style={styles.cardStatsItemText}>
+                                  {type.name}
+                                </Text>
+                              </View>
+                            ))}
+                          </View>
+                        </View>
+
+                        <View style={styles.cardFooter}>
+                          <Text style={styles.cardFooterText}>
+                            Height: {height}
+                          </Text>
+
+                          <Text style={styles.cardFooterText}>
+                            Weight: {weight}
+                          </Text>
+                        </View>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                );
               },
-              index,
-            ) => {
-              return (
-                <TouchableOpacity
-                  key={index}
-                  onPress={() => {
-                    // handle onPress
-                  }}
-                >
-                  <View style={styles.card}>
-                    <View style={styles.cardTop}>
-                      <Image
-                        alt=""
-                        resizeMode="cover"
-                        style={styles.cardImg}
-                        source={{ uri: sprites?.regular }}
-                      />
-                    </View>
-
-                    <View style={styles.cardBody}>
-                      <View style={styles.cardHeader}>
-                        <Text style={styles.cardTitle}>{name?.fr}</Text>
-
-                        <Text style={styles.cardPrice}>
-                          Pokedex ID: {pokedex_id}
-                        </Text>
-                      </View>
-
-                      <View style={styles.cardStats}>
-                        <View style={styles.cardStatsItem}>
-                          <FeatherIcon color="#48496c" name="zap" size={14} />
-
-                          <Text style={styles.cardStatsItemText}>
-                            Generation: {generation}
-                          </Text>
-                        </View>
-
-                        <View style={styles.cardStatsItem}>
-                          {types?.map((type, index) => (
-                            <View
-                              key={index}
-                              style={{
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                marginRight: 3,
-                                marginLeft: 3,
-                              }}
-                            >
-                              <Image
-                                source={{ uri: type.image }}
-                                style={styles.cardTypeImg}
-                              />
-                              <Text style={styles.cardStatsItemText}>
-                                {type.name}
-                              </Text>
-                            </View>
-                          ))}
-                        </View>
-
-                        {/* <View style={styles.cardStatsItem}>
-                          <FeatherIcon color="#48496c" name="clock" size={14} />
-
-                          <Text style={styles.cardStatsItemText}>
-                            Forme: {forme}
-                          </Text>
-                        </View> */}
-                      </View>
-
-                      <View style={styles.cardFooter}>
-                        <Text style={styles.cardFooterText}>
-                          Height: {height}
-                        </Text>
-
-                        <Text style={styles.cardFooterText}>
-                          Weight: {weight}
-                        </Text>
-                      </View>
-                    </View>
-                  </View>
-                </TouchableOpacity>
-              );
-            },
-          )}
+            )}
         </ScrollView>
       )}
     </SafeAreaView>
